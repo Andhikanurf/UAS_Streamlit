49a949b5acda3a569a0f6893338c20a463876105
Andhikanurf <97925983+Andhikanurf@users.noreply.github.com>
2023-02-02 01:06:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -14,12 +14,10 @@ img = Image.open ('iris.png')
 st.image(img, use_column_width=False)
 st.write("ISI SEMUA NILAI DIBAWAH INI UNTUK MENDAPATKAN PREDIKSI:")
 
-st.sidebar.header('PARAMETER iNPUTAN')
-
-SepalLengthCm = st.sidebar.slider('SepalLengthCm:', 2.0, 6.0)
-SepalWidthCm = st.sidebar.slider('SepalWidthCm:', 0.0, 5.0)
-PetalLengthCm = st.sidebar.slider('PetalLengthCm',0.0, 3.0)
-PetalWidthCm = st.sidebar.slider('PetalWidthCm:', 0.0, 2.0)
+SepalLengthCm = st.slider('SepalLengthCm:', 2.0, 6.0)
+SepalWidthCm = st.slider('SepalWidthCm:', 0.0, 5.0)
+PetalLengthCm = st.slider('PetalLengthCm',0.0, 3.0)
+PetalWidthCm = st.slider('PetalWidthCm:', 0.0, 2.0)
 data = {'SepalLengthCm': SepalLengthCm,
         'SepalWidthCm': SepalWidthCm,
         'PetalLengthCm': PetalLengthCm,
