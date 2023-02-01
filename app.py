27dd75216d55b683f099db360e2038446a4792be
Andhikanurf <97925983+Andhikanurf@users.noreply.github.com>
2023-02-02 01:04:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,14 +7,14 @@ from sklearn.svm import SVC
 
 model = pickle.load(open('model.pkl', 'rb'))
 
-st.header("IRIS CLASSIFICATION PREDICTION")
+st.header("IRIS CLASSIFICATION")
 st.write("ANDHIKA NUR FIRDAUS -- 2019230061")
 
 img = Image.open ('iris.png')
 st.image(img, use_column_width=False)
-st.write("Please Insert Values, to Get Iris Class prediction:")
+st.write("ISI SEMUA NILAI DIBAWAH INI UNTUK MENDAPATKAN PREDIKSI:")
 
-st.sidebar.header('Parameter Value Prediction')
+st.sidebar.header('PARAMETER iNPUTAN')
 
 SepalLengthCm = st.sidebar.slider('SepalLengthCm:', 2.0, 6.0)
 SepalWidthCm = st.sidebar.slider('SepalWidthCm:', 0.0, 5.0)
@@ -27,5 +27,5 @@ data = {'SepalLengthCm': SepalLengthCm,
 
 features = pd.DataFrame(data, index=[0])
 
-st.subheader('Parameter Inputan')
+st.subheader('PARAMETER INPUTAN')
 st.write(features)
